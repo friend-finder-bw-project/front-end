f9ec6d1582f34f6fd4c09fed59814892cc79a90f
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-18 10:53:36 +0000

    Basic login form completed without axios

---
+++ friend-finder/src/components/LoginForm.js
@@ -0,0 +1,61 @@
+// Colin and Luis
+import React from 'react';
+import ReactDom from 'react-dom';
+import {withFormik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from "yup";
+
+
+
+
+
+function LoginForm() {
+  return (
+    <div>
+        <h1>Login Screen</h1>
+        <Form>
+            <label>
+                User Name: 
+                
+                <Field 
+                type= "text"
+                name= "userName"
+                placeholder= "userName"
+                />
+
+                
+            </label>
+
+            <label>
+               Password: 
+                
+                <Field 
+                type= "password"
+                name= "Password"
+                placeholder= "Password"
+                />
+
+                
+                
+            </label>
+            <input type="submit"/>
+        </Form>
+    </div>
+  );
+
+}
+
+const LoginFormWithFormik= withFormik ({
+    mapPropsToValues(){
+        return {
+            userName:"",
+            password:"",
+        };
+    },
+validationSchema: Yup.object().shape({
+    userName: Yup.string().required(""),
+    password: Yup.string().required(""),
+})
+
+})  (LoginForm);
+
+export default LoginFormWithFormik;
