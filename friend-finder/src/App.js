45841a1bbf66067130cc4820a6918d295ac21d39
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-18 13:42:55 +0000

    Draft Navigation added

--- friend-finder/src/App.js
+++ friend-finder/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Nav from './components/Nav';
 import LoginForm from './components/LoginForm';
 import SignUp from './components/SignUp';
 import Survey from './components/Survey'
@@ -8,6 +9,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <Nav />
     <Route exact path = '/' component={SignUp}/>
     <Route exact path = '/login' component={LoginForm}/>
     <Route exact path = '/survey' component={Survey}/>
