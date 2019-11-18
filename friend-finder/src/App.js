181f20f0521d2ae7184755f1e5a31e865579c16d
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-18 11:02:40 +0000

    Development Branch for merges

--- friend-finder/src/App.js
+++ friend-finder/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoginForm from './components/LoginForm';
+import SignUp from './components/SignUp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <SignUp />
     <LoginForm />
     </div>
   );
