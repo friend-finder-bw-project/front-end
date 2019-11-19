24bdabc34f828ea295d558cc97b6bc05e0f4f370
Luis Schekerka <luis@Luiss-MBP.fritz.box>
2019-11-19 14:21:12 +0100

    authentification built by Mildred

--- friend-finder/src/App.js
+++ friend-finder/src/App.js
@@ -1,6 +1,6 @@
 // Colin and Luis
 
-import React from 'react';
+import React, {useState} from 'react';
 import { Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import LoginForm from './components/LoginForm';
@@ -9,16 +9,22 @@ import Survey from './components/Survey';
 import Profile from './components/Profile';
 import FriendsList from './components/FriendsList';
 import './App.css';
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
+const [formValues,setFormValues] = useState({
+  "username": "",
+  "password":""
+})
   return (
     <div className="App">
     <Nav />
 
-    <Route exact path = '/' component={SignUp}/>
+    <Route exact path = '/' render={props => <SignUp formValues={formValues} setFormValues={setFormValues} {...props}/>}/>
     <Route exact path = '/login' component={LoginForm}/>
+    <Route  path="/profile" component={Profile}/>
     <Route exact path = '/survey' component={Survey}/>
-    <Route exact path = '/profile' component={Profile}/>
+    {/* <Route exact path = '/profile' component={Profile}/> */}
     <Route exact path = '/friendslist' component={FriendsList}/>
 
     </div>
--- friend-finder/src/components/LoginForm.js
+++ friend-finder/src/components/LoginForm.js
@@ -5,7 +5,38 @@ import ReactDom from 'react-dom';
 import { withFormik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from "yup";
 
-function LoginForm() {
+function LoginForm(props) {
+    // console.log(props);
+    // const {formV}
+    // const handleChange = e => {
+    //     setFormValues({ ...formValues, [e.target.name]: e.target.value });
+    //   };
+    //   const handleSubmit = e => {
+    //       e.preventDefault();
+    //       axios.post("http://localhost:3300/api/auth/login",formValues)
+    //       .then(res => {
+    //           console.log(res);
+    //           props.history.push("/jokes");
+    //          window.localStorage.setItem("token",res.data.token);
+    //       })
+    //       .catch(err => {
+    //           console.log(err);
+    //       })
+    //   }
+    //   console.log("form values", formValues);
+    //   return (
+    //     <div>
+    //       <form>
+    //         <input placeholder="username" name="username" onChange={handleChange} />
+           
+    //         <input placeholder="password" name="password" onChange={handleChange} />
+    //         <button onClick={handleSubmit} >Submit</button>
+    //       </form>
+    //     </div>
+    //   );
+    // }
+    
+    // export default Login;
     return (
         <div>
             <h1>Login Screen</h1>
